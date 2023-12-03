37629061b2d0f65be5a67a846079b243724e5634
LueKely <99306129+LueKely@users.noreply.github.com>
2023-12-03 12:22:46 +0800

    day-1: finished

--- 2023/day-01/day-01.mjs
+++ 2023/day-01/day-01.mjs
@@ -2,4 +2,26 @@ import { readFile } from '../utils/readTextFile.mjs';
 
 const payload = readFile('./day-01.txt');
 
-console.log(payload);
+function getFindNumber(input) {
+	const r = /(\d+)/;
+
+	let result = '';
+
+	for (let i = 0; i < input.length; i++) {
+		if (r.test(input[i])) {
+			result += input[i];
+		}
+	}
+
+	return Number(`${result[0]}${result[result.length - 1]}`);
+}
+
+const convert = payload.map((item) => {
+	return getFindNumber(item);
+});
+
+console.log(convert);
+
+// add all numbers in array
+const sum = convert.reduce((a, b) => a + b, 0);
+console.log(sum);
