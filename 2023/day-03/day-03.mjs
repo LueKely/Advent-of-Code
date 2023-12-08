2e918ab9ed2931a0fa0ec5c653abdbeb25808a55
LueKely <luekely43110@gmail.com>
2023-12-08 22:44:27 +0800

    i did something

--- 2023/day-03/day-03.mjs
+++ 2023/day-03/day-03.mjs
@@ -1,1 +1,35 @@
-console.log('TEST');
+import { readFile } from '../utils/readTextFile.mjs';
+
+const payload = readFile('./day-03.txt');
+// let's try string manipulation for this part
+// such that we can compare strings insted of turning them into arrays
+
+const segment = payload[0];
+console.log(segment);
+const stack = [];
+const regex = /\d+/g;
+let part = '';
+
+for (let index = 0; index < segment.length; index++) {
+	const next = segment[index + 1];
+	const matches = segment[index].match(regex);
+	// const matchnNext = segment[next].match(regex);
+
+	if (matches) {
+		part += segment[index];
+
+		if (index !== segment.length - 1) {
+			if (!next.match(regex)) {
+				stack.push(part);
+				part = '';
+			}
+		}
+	}
+
+	// if (matchnNext) {
+
+	// }
+}
+
+console.log(segment.length);
+console.log(stack);
