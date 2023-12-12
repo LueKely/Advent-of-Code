1663384eed5d6918a2a2c4d34cc4154008485205
LueKely <luekely43110@gmail.com>
2023-12-12 22:20:44 +0800

    moses algorithm

--- 2023/day-03/day-03.mjs
+++ 2023/day-03/day-03.mjs
@@ -18,10 +18,7 @@ function readLine(line) {
 }
 // todo please work on this return only one array nalang thanks lue
 function getPostionsHorizontal(symbolPos) {
-	return {
-		topBottom: [symbolPos - 3, symbolPos + 4],
-		leftRight: [symbolPos - 3, symbolPos + 4],
-	};
+	return [symbolPos - 3, symbolPos + 4];
 }
 
 // create a line cutter that will take an numbers on which part it will slice
@@ -36,12 +33,10 @@ function lineCutter(position, line) {
 // rework line parser
 function lineParser(line) {
 	const stack = [];
-
 	const regex = /[^0-9a-zA-Z.]/g;
 	const regexNumber = /^\d+$/;
 	const regexMiddleDot = /\d\.\d/;
-	let part = '';
-	// this if is for the middle
+	// this if is for the middle arrat
 	if (regex.test(line[3])) {
 		const left = line.slice(0, 3);
 		const right = line.slice(4, 7);
@@ -62,17 +57,50 @@ function lineParser(line) {
 			}
 		}
 	} else {
-		console.log('bar');
+		let build = '';
+		// if middle starts with a number
+		if (regexNumber.test(line[3])) {
+			// if 0011100
+			if (regexNumber.test(line[2]) && regexNumber.test(line[4])) {
+				build = line[2] + line[3] + line[4];
+				stack.push(build);
+			}
+			// if ?11000
+			else if (regexNumber.test(line[2])) {
+				//if 0011000
+				if (!regexNumber.test(line[1])) {
+					build = line[2] + line[3];
+					stack.push(build);
+				}
+				// if 0111000
+				else {
+					build = line[1] + line[2] + line[3];
+					stack.push(build);
+				}
+			}
+			// if 00011?
+			else if (regexNumber.test(line[4])) {
+				if (regexNumber.test(line[5])) {
+					build = line[3] + line[4] + line[5];
+					stack.push(build);
+				} else {
+					build = line[3] + line[4];
+					stack.push(build);
+				}
+			} else {
+				stack.push(line[3]);
+			}
+		}
 	}
 
 	return stack;
 }
 
-const topSegment = payload[9];
-const segment = payload[10];
-const bottomSegment = payload[11];
+const topSegment = payload[107];
+const segment = payload[108];
+const bottomSegment = payload[109];
 
-const normalizedPositions = getPostionsHorizontal(39);
+const normalizedPositions = getPostionsHorizontal(12);
 
 // control group
 console.log(bottomSegment);
@@ -82,17 +110,22 @@ console.log(topSegment);
 console.log('normalized position based on the current symbol position');
 console.log(normalizedPositions);
 
-const topResult = lineCutter(normalizedPositions.topBottom, topSegment);
-const bottomResult = lineCutter(normalizedPositions.topBottom, bottomSegment);
-const leftAndRightResult = lineCutter(normalizedPositions.leftRight, segment);
+const topResult = lineCutter(normalizedPositions, topSegment);
+const bottomResult = lineCutter(normalizedPositions, bottomSegment);
+const leftAndRightResult = lineCutter(normalizedPositions, segment);
 
 // top
-console.log('top: ' + topResult);
+console.log('top:    ' + topResult);
+// middle
+console.log('middle: ' + leftAndRightResult);
 // bottom
 console.log('bottom: ' + bottomResult);
-// left
-console.log('left and right ' + leftAndRightResult);
-// right
+
+console.log('middle:');
 console.log(lineParser(leftAndRightResult));
+console.log('top:');
+console.log(lineParser(topResult));
+console.log('bottom');
+console.log(lineParser(bottomResult));
 // console.log(lineParser(segment));
 // readLine(segment);
